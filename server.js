78aae249ff0246f129ec7575e001e897230ee745
Sarun Tapee <tpzrun@gmail.com>
2021-09-09 18:07:06 +0700

    connect to DB

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 // load env var
 dotenv.config({ path: "./config/config.env" });
 const PORT = process.env.PORT || "8080";
+
+connectDB();
 
 // route files
 const bootcamps = require("./routes/bootcamps.js");
@@ -19,7 +22,14 @@ if (process.env.NODE_ENV === "development") {
 // mount routers
 server.use("/api/v1/bootcamps", bootcamps);
 
-server.listen(
+const app = server.listen(
     PORT, 
     () => console.log(`Serving in ${process.env.NODE_ENV} mode on port ${PORT}`) 
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server and exit process
+    app.close(() => process.exit(1));
+});
