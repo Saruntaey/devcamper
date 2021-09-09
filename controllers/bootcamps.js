737b7b044484dcb16dba418aa0d9335296fabb8b
Sarun Tapee <tpzrun@gmail.com>
2021-09-09 21:11:50 +0700

    implement update and delete bootcamp controller

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -6,7 +6,7 @@ const Bootcamp = require("../models/bootcamp");
 exports.getBootcamps = async (req, res, next) => {
     try {
         const bootcamp = await Bootcamp.find();
-        res.status(200).json({ success: true, data: bootcamp });
+        res.status(200).json({ success: true, count:bootcamp.length, data: bootcamp });
     } catch (err) {
         res.status(400).json({ success: false, data: null, error: err });
     }
@@ -42,13 +42,32 @@ exports.createBootcamp = async (req, res, next) => {
 // @desc    Update bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-exports.updateBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+exports.updateBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        if (!bootcamp) {
+            return res.status(404).json({ success: false, data: null });
+        }
+        res.status(200).json({ success: true, data: bootcamp });
+    } catch (err) {
+        res.status(400).json({ success: false, data: null, error: err });
+    }
 }
 
 // @desc    Delete bootcamp
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
-exports.deleteBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+exports.deleteBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+        if (!bootcamp) {
+            return res.status(404).json({ success: false, data: null });
+        }
+        res.status(200).json({ success: true, data: bootcamp });
+    } catch (err) {
+        res.status(400).json({ success: false, data: null, error: err });
+    }
 }
