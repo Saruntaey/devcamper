7b4ef3dba6aed515b9b3db684f16a80c8d02297b
Sarun Tapee <tpzrun@gmail.com>
2021-09-11 21:36:03 +0700

    add forgot password route

--- controllers/auth.js
+++ controllers/auth.js
@@ -39,6 +39,38 @@ exports.login = asyncHandeler(async (req, res, next) => {
     sendTokenResponse(user, 200, res);
 });
 
+// @desc    Get current logged in user
+// @route   GET /api/v1/auth/me
+// @access  Private
+exports.getMe = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        data: req.user
+    });
+};
+
+// @desc    Forgot password
+// @route   POST /api/v1/auth/forgotpassword
+// @access  Public
+exports.forgotPassword = asyncHandeler(async (req, res, next) => {
+    const user = await User.findOne({email: req.body.email});
+
+    if (!user) {
+        return next(new ErrorResponse(`There is no user with email ${req.body.email}`, 404));
+    }
+
+    // get reset token
+    const resetToken = user.getResetPasswordToken();
+
+    await user.save({ validateBeforeSave: false });
+
+    res.status(200).json({
+        success: true,
+        data: user
+    });
+});
+
+
 // get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
     // create token
@@ -60,14 +92,4 @@ const sendTokenResponse = (user, statusCode, res) => {
             success: true,
             token
         })
-};
-
-// @desc    Get current logged in user
-// @route   GET /api/v1/auth/me
-// @access  Private
-exports.getMe = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        data: req.user
-    });
 };
--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 const Schema = mongoose.Schema;
 
@@ -39,6 +40,9 @@ const userSchema = new Schema({
 
 // encrypt password with bcrypt
 userSchema.pre("save", async function(next) {
+    if (!this.isModified("password")) {
+        next();
+    }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
     next();
@@ -54,6 +58,20 @@ userSchema.methods.getSignedJwtToken = function() {
 // match user enter password to hashed password in database
 userSchema.methods.matchPassword = async function(enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
+}
+
+// generate and hash password token
+userSchema.methods.getResetPasswordToken = function() {
+    // generate token
+    const resetToken = crypto.randomBytes(20).toString("hex");
+
+    // hash token and set to resetPassword field
+    this.resetPasswordToken = crypto.createHash("sha256").update(resetToken).digest("hex");
+
+    // set expire
+    this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+
+    return resetToken;
 }
 
 module.exports = mongoose.model("User", userSchema);
