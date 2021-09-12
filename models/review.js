09b679b195cbaa8743e2082b0e24a306aca37369
Sarun Tapee <tpzrun@gmail.com>
2021-09-12 12:45:31 +0700

    average rating for bootcamp before and after save

--- models/review.js
+++ models/review.js
@@ -39,4 +39,37 @@ const reviewSchema = new Schema({
 // prevent user from submittin more than one review per bootcamp
 reviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
 
+// static method to get avg of rating and save
+reviewSchema.statics.getAverageRating = async function (bootcampId) {    
+    const obj = await this.aggregate([
+        {
+            $match: {bootcamp: bootcampId}
+        },
+        {
+            $group: {
+                _id: "$bootcamp",
+                averageCost: {$avg: "$rating"}
+            }
+        }
+    ]);
+    
+    try {
+        await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+            averageRating: obj[0].averageCost
+        })
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+// call getAverageRating after save
+reviewSchema.post("save", async function() {
+    await this.constructor.getAverageRating(this.bootcamp);
+});
+
+// call getAverageRating after remove
+reviewSchema.post("remove", async function() {
+    await this.constructor.getAverageRating(this.bootcamp);
+})
+
 module.exports = mongoose.model("Review", reviewSchema);
